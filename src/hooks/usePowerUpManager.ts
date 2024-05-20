2c405356ca2381341ede9ec391c240c4535c1ac5
Alidin Abylkasym Uulu <nrudxe@inf.elte.hu>
2024-05-20 12:49:41 +0200

    adding mutual exclusive powerups

--- src/hooks/usePowerUpManager.ts
+++ src/hooks/usePowerUpManager.ts
@@ -1,14 +1,30 @@
+/* eslint-disable quote-props */
+/* eslint-disable consistent-return */
 /* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useCallback } from 'react';
 import { Power } from '../model/gameItem';
-import { Player } from '../model/player';
 
 const usePowerUpManager = () => {
   const [activePowerUps, setActivePowerUps] = useState<Record<string, Set<Power>>>({});
   const [flashingPowerUps, setFlashingPowerUps] = useState<Record<string, Set<Power>>>({});
 
   const addPowerUp = useCallback((playerId: string, powerUp: Power, duration: number) => {
+    const mutuallyExclusivePowerUps: Record<Power, Power | null> = {
+      'Ghost': 'Invincibility',
+      'Invincibility': 'Ghost',
+      'AddBomb': null,
+      'BlastRangeUp': null,
+      'Detonator': null,
+      'RollerSkate': null,
+      'Obstacle': null
+    };
+
+    const currentActivePowerUps = activePowerUps[playerId] || new Set();
+    const exclusivePowerUp = mutuallyExclusivePowerUps[powerUp];
+    if (exclusivePowerUp !== null && currentActivePowerUps.has(exclusivePowerUp)) {
+      return;
+    }
     setActivePowerUps((prev) => ({
       ...prev,
       [playerId]: new Set([...(prev[playerId] || []), powerUp]),
@@ -38,7 +54,7 @@ const usePowerUpManager = () => {
     }, duration - 3000);
 
     return () => clearTimeout(timeout);
-  }, []);
+  }, [activePowerUps]);
 
   const removePowerUp = useCallback((playerId: string, powerUp: Power) => {
     setActivePowerUps((prev) => {
